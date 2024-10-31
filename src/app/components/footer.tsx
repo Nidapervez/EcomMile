274f27fc40f3887dfbc32f6b88cf231ec3b0fef2
Nida <nnpervez333@gmail.com>
2024-10-31 11:57:17 -0700

    change

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,172 +1,69 @@
-import React from 'react';
+import Image from "next/image";
+import image from "@/app/public/images/nidz.jpg"; 
 
-const Footer = () => {
-  return (
-    <div>
-      <footer className="text-gray-100 body-font bg-black">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap md:text-left text-center -mb-10 -mx-4 justify-between">
-            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-100 tracking-widest text-sm mb-3">
-                Company
-              </h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Home</a>
-                </li>
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">About</a>
-                </li>
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Contact us</a>
-                </li>
-              </nav>
-            </div>
-            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-100 tracking-widest text-sm mb-3">
-                Services
-              </h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Branding</a>
-                </li>
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Design</a>
-                </li>
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Marketing</a>
-                </li>
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Advertisement</a>
-                </li>
-              </nav>
-            </div>
-            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-100 tracking-widest text-sm mb-3">
-                Legal
-              </h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Term of use</a>
-                </li>
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Privacy Policy</a>
-                </li>
-                <li>
-                  <a className="text-gray-200 hover:text-gray-100">Cookie Policy</a>
-                </li>
-              </nav>
-            </div>
-            <div className="lg:w-1/6 md:w-1/2 w-full mr-10 flex "> {/* Adjusted container */}
-              <div className="relative w-full">
-                <label
-                  htmlFor="footer-field"
-                  className="leading-7 text-sm text-gray-100"
-                >
-                  Subscribe
-                </label>
-                <input
-                  type="text"
-                  id="footer-field"
-                  name="footer-field"
-                  className="flex flex-col w-48 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out "
-                />
-              
-              <button className="flex flex-col text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded w-60 mt-6">
-            Notifications
-              </button></div>
-            </div>
-          </div>
-        </div>
-        <div className="border-t border-gray-200">
-          <div className="container px-5 py-8 flex flex-wrap mx-auto items-center">
-            <div className="flex md:flex-nowrap flex-wrap justify-center items-end md:justify-start">
-              <p className="text-gray-100 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">
-                Bitters chicharrones fanny pack
-                <br className="lg:block hidden" />
-                waistcoat green juice
-              </p>
-            </div>
-            <span className="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
-              <a className="text-gray-500">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-500">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-500">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
-                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-500">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={0}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="none"
-                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                  />
-                  <circle cx={4} cy={4} r={2} stroke="none" />
-                </svg>
-              </a>
-            </span>
-          </div>
-        </div>
-        <div className="bg-gray-100">
-          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p className="text-gray-500 text-sm text-center sm:text-left">
-              © 2020 Tailblocks —
-              <a
-                href="https://twitter.com/knyttneve"
-                className="text-gray-600 ml-1"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                @knyttneve
-              </a>
+
+export default function Footer() {
+    return (
+      <footer className="text-white body-font bg-black">
+        <div className="container px-5 py-16 mx-auto flex flex-col md:flex-row md:items-start">
+          <div className="w-full md:w-1/4 flex-shrink-0 mx-auto text-center md:text-left mb-10 md:mb-0">
+            <a className="flex title-font font-medium items-center justify-center md:justify-start text-white">
+            <Image src={image} alt="Blog Image" height={50} width={50} />
+              <span className="ml-3 text-xl">NidzWeb</span>
+            </a>
+            <p className="mt-2 text-sm text-gray-500">
+              Your go-to place for web solutions.
             </p>
-            <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">
-              Enamel pin tousled raclette tacos irony
-            </span>
+          </div>
+          
+          <div className="flex-grow flex flex-wrap md:pl-20 ">
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 ">
+              <h2 className="title-font font-medium text-white tracking-widest  mb-3 text-3xl font-mono">
+                LINKS
+              </h2>
+              <nav className="flex space-x-16 mb-10  text-3xl font-mono ">
+                <li>
+                  <a 
+                    href="https://second-mile-stone-96sf.vercel.app/project" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="text-white hover:text-gray-800"
+                  >
+                    Portfolio
+                  </a>
+                </li>
+                <li>
+                  <a 
+                    href="https://github.com/Nidapervez" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="text-white hover:text-gray-800"
+                  >
+                    GitHub
+                  </a>
+                </li>
+                <li>
+                  <a 
+                    href="https://www.linkedin.com/in/nida-pervez-8468202b6/" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="text-white hover:text-gray-800"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-200">
+          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className="text-white text-sm text-center sm:text-left">
+              Contact: <a href="mailto:nnpervez333@gmail.com" className="text-gray-600 hover:text-gray-800">nnpervez333@gmail.com</a>
+            </p>
           </div>
         </div>
       </footer>
-    </div>
-  );
-}
-
-export default Footer;
+    );
+  }
+  
