dee49aa5932583e9218a8d72a03708b82deafb4f
Nida <nnpervez333@gmail.com>
2024-10-31 08:46:13 -0700

    change

--- src/app/components/productList.tsx
+++ src/app/components/productList.tsx
@@ -4,6 +4,7 @@ import { useAppSelector, useAppDispatch } from '../store/hooks';
 import { addCart } from '../store/slices/cart';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Image from 'next/image';
 
 const ProductList = () => {
     const products = useAppSelector(state => state.productArray);
@@ -22,7 +23,7 @@ const ProductList = () => {
                         key={i} 
                         className="border border-gray-200 rounded-lg overflow-hidden shadow-lg transform hover:scale-105 transition duration-300 bg-white"
                     >
-                        <img 
+                        <Image 
                             src={item.image} 
                             alt={item.name} 
                             className="w-full h-64 object-cover"
